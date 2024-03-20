3bafe62ef767bc26d66c17b13de3654922512e95
seungmin lee <sm.lee@muzlive.com>
2024-03-20 13:58:38 +0900

    환경셋팅

---
+++ src/main/kotlin/com/whoelse/WhoelseApplication.kt
@@ -0,0 +1,11 @@
+package com.whoelse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhoelseApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhoelseApplication>(*args)
+}
