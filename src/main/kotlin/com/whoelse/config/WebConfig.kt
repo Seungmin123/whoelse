cd8169da9b99c1952056b173b6aaa07f00bdd2da
seungmin lee <sm.lee@muzlive.com>
2024-04-19 18:40:42 +0900

    feat_common_config - WebConfig 환경셋팅

--- src/main/kotlin/com/whoelse/config/WebConfig.kt
+++ src/main/kotlin/com/whoelse/config/WebConfig.kt
@@ -1,4 +1,12 @@
 package com.whoelse.config
 
-class WebConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer{
+
+//    override fun addInterceptors(registry: InterceptorRegistry) {
+//    }
 }
